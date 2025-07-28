5307eeae022a4059c74ccc613ebfa6bcb31bfea8
Ricardo Chavez <chavezmataricardo@gmail.com>
2025-07-28 14:45:08 -0400

    Initialize Vuetify project with essential configurations and files. Added .browserslistrc, .editorconfig, .gitignore, and package.json for project setup. Included Vite configuration, TypeScript settings, and basic Vue structure with App.vue and router setup. Added README for project documentation and component auto-importing. Included Vuetify plugin integration and basic styles configuration.

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-center text-red">Hello World</h1>
+    <p class="text-center text-blue">This is a test</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
